23142427efa9155c30c6e982313b112dff4d1fae
Leonardo <leonardolp@gmail.com>
2021-05-19 08:22:42 -0300

    atualizando

--- build/modulo04/modulo04.js
+++ build/modulo04/modulo04.js
@@ -5,9 +5,12 @@ function addNumbers(x, y) {
 addNumbers(1, 2);
 /******************* */
 let addNew = function (x, y) {
+    if (y === undefined)
+        return x;
     return x + y;
 };
-addNew(1, 2);
+console.log('aqui 0', addNew(1, 2));
+console.log('aqui 1', addNew(12));
 /*************** */
 //  let total = function (input: number[]): number {
 let total = (input) => {
@@ -21,3 +24,34 @@ let total = (input) => {
     return total;
 };
 console.log(total([1, 2, 3, 4]));
+/************* */
+let addAllNumbers = (firstNumber, ...restOfNumbers) => {
+    let total = firstNumber;
+    for (let counter = 0; counter < restOfNumbers.length; counter++) {
+        if (isNaN(restOfNumbers[counter])) {
+            continue;
+        }
+        total += Number(restOfNumbers[counter]);
+    }
+    return total;
+};
+console.log(addAllNumbers(1, 2, 3, 4, 5, 6, 7));
+console.log(addAllNumbers(2));
+function displayMessage({ text, sender }) {
+    console.log(`Message from ${sender}: ${text}`);
+}
+displayMessage({ sender: 'Christopher', text: 'hello, world' });
+let addNumbers = (x, y) => x + y;
+let subtractNumbers = (x, y) => x - y;
+console.log(addNumbers(1, 2));
+console.log(subtractNumbers(1, 2));
+let doCalculation = (operation) => {
+    if (operation === 'add') {
+        return addNumbers;
+    }
+    else {
+        return subtractNumbers;
+    }
+};
+console.log(doCalculation('add')(1, 2));
+console.log(doCalculation('subtract')(1, 2));
--- modulo04/modulo04.ts
+++ modulo04/modulo04.ts
@@ -55,3 +55,24 @@ interface Message {
  }
  
  displayMessage({sender: 'Christopher', text: 'hello, world'});
+
+ /**************** */
+
+ type calculator = (x: number, y: number) => number;
+
+let addNumbers: calculator = (x: number, y: number): number => x + y;
+let subtractNumbers: calculator = (x: number, y: number): number => x - y;
+
+console.log(addNumbers(1, 2));
+console.log(subtractNumbers(1, 2));
+
+let doCalculation = (operation: 'add' | 'subtract'): calculator => {
+    if (operation === 'add') {
+        return addNumbers;
+    } else {
+        return subtractNumbers;
+    }
+}
+
+console.log(doCalculation('add')(1, 2))
+console.log(doCalculation('subtract')(1, 2))
