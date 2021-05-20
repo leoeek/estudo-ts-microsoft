fec6e06ea6692e6942852ce770b6a16be0d17ff8
Leonardo <leonardolp@gmail.com>
2021-05-20 08:36:25 -0300

    generic

---
+++ modulo06/modulo06.ts
@@ -0,0 +1,41 @@
+type ValidTypes = string | number;
+function identity<T extends ValidTypes, U> (value: T, message: U) {   // Return type is inferred
+    let result: ValidTypes = '';
+    let typeValue: string = typeof value;
+
+    if (typeof value === 'number') {           // Is it a number?
+        result = value + value;                // OK
+    } else if (typeof value === 'string') {    // Is it a string?
+        result = value + value;                // OK
+    }
+
+    console.log(`The message is ${message} and the function returns a ${typeValue} value of ${result}`);
+
+    return result
+}
+
+let numberValue = identity<number, string>(100, 'Hello');
+let stringValue = identity<string, string>('100', 'Hello');
+
+console.log(numberValue);       // Returns 200
+console.log(stringValue);       // Returns 100100
+
+/************* */
+
+interface Identify<T, U> {
+    value: T;
+    message: U;
+}
+
+let returnNumber: Identify<number, string> = {
+    value: 25,
+    message: 'Hello'
+}
+
+let returnString: Identify<string, number> = {
+    value: 'Hello!',
+    message: 25
+}
+
+/************************ */
+
